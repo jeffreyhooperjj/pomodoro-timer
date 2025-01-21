7d8f07c6c75096933d3cc7ded30def284531fabc
jeffreyhooperjj <jeffreyrepooh@gmail.com>
2025-01-21 08:59:38 -0800

    Initial Commit

---
+++ pomodoro task timer/pomodoro_task_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoro_task_timerApp.swift
+//  pomodoro task timer
+//
+//  Created by Jeffrey Hooper on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct pomodoro_task_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
